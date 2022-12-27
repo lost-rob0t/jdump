6750ac699bb3fed304b5f571bd09f57af8311c97
nsaspy <nsaspy@airmail.cc>
2022-12-27 00:08:58 -0500

    Added files

---
+++ jdump.nimble
@@ -0,0 +1,14 @@
+# Package
+
+version       = "0.1.0"
+author        = "nsaspy"
+description   = "View and concat json files"
+license       = "MIT"
+srcDir        = "src"
+bin           = @["jdump"]
+
+
+# Dependencies
+
+requires "nim >= 1.6.10"
+requires "cligen"
---
+++ src/jdump.nim
@@ -0,0 +1,46 @@
+import json
+import strutils
+import tables
+import strformat
+import terminal
+proc format(doc: JsonNode, indent: int = 0, str: var string = ""): string =
+  if str == "":
+    str = "====\n"
+  let pairs = doc.getFields
+  for key in pairs.keys:
+    case pairs[key].kind:
+      of JObject:
+        var nstr = "\n" & indent(fmt"==={key}===:", indent) & "\n"
+        var str1 = pairs[key].format(indent + 2, nstr)
+        str &= str1
+      else:
+        str &= indent(fmt"{key}: "  & $pairs[key], indent) & "\n"
+  result = str
+
+#proc fetchUrl(url: string, headers: Table[string, string] = newTab): JsonNode
+
+proc main(mode: string = "file", input: seq[string]) =
+  if isatty(stdin):
+    for file in input:
+      let o = open(file, fmRead)
+      for line in o.lines:
+        var t = ""
+        try:
+          echo format(line.parseJson, 2, t)
+        except JsonParsingError:
+          discard
+          when defined(debug):
+            echo line
+      o.close
+  else:
+    for line in stdin.lines:
+      var t = ""
+      try:
+        echo format(line.parseJson, 2, t)
+      except JsonParsingError:
+        discard
+        when defined(debug):
+          echo line
+
+when isMainModule:
+  import cligen; dispatch main, help={"mode": "Get json from file or web", "input": "list of files or http urls"}
